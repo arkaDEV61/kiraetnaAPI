e47c2b1ec12b0dd780ef398104ea240daf300e39
Armin Causevic <armin61@gmail.com>
2023-07-14 09:03:11 +0200

    removed unused packages strapi upgrade
    Took 8 minutes

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      schema: process.env.DATABASE_SCHEMA, // Not required
-      rejectUnauthorized: env.bool(
-                  'DATABASE_SSL_SELF',
-                  false
-          ),
-    },
-  },
-});
+ module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'bank'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', '0000'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL', false),
+          },
+        },
+        debug: false,
+      },
+    });
